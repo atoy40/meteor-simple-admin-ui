690394190a4535ffad32313ae5c4cbb61cb35133
Cyrille Colin <cyrille.colin@eskan.fr>
2014-02-19 18:08:10 +0100

    add projections to allUsers publish / fix indentation

--- simple_admin_ui_server.js
+++ simple_admin_ui_server.js
@@ -1,14 +1,15 @@
 Meteor.publish("allUsers", function() {
-	if (Roles.userIsInRole(this.userId, ['admin'])) {
-		return 
-		[
-			Meteor.users.find({}, { username : 1,
-								   profile : 1,
-								   roles : 1,
-								   emails : 1,								  
-								   sort: {'profile.name': 1}}),
-			Meteor.roles.find({})
-		];
-	}
-	return null;
+  if (Roles.userIsInRole(this.userId, ['admin'])) {
+    return 
+    [
+      Meteor.users.find({}, { 
+        username : 1,
+        profile : 1,
+        roles : 1,
+        emails : 1,								  
+        sort: {'profile.name': 1}}),
+      Meteor.roles.find({})
+    ];
+  }
+  return null;
 });
