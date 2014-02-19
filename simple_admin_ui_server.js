30dc3800ab8c4f943b82089f263cfc8520a6dea8
Cyrille Colin <cyrille.colin@eskan.fr>
2014-02-19 17:53:09 +0100

    add projections to allUsers publish

--- simple_admin_ui_server.js
+++ simple_admin_ui_server.js
@@ -1,11 +1,14 @@
 Meteor.publish("allUsers", function() {
-  if (Roles.userIsInRole(this.userId, ['admin'])) {
-    return Meteor.users.find({}, {sort: {'profile.name': 1}});
-  }
-
-  return null;
-});
-
-Meteor.publish(null, function (){ 
-  return Meteor.roles.find({})
+	if (Roles.userIsInRole(this.userId, ['admin'])) {
+		return 
+		[
+			Meteor.users.find({}, { username : 1,
+								   profile : 1,
+								   roles : 1,
+								   emails : 1,								  
+								   sort: {'profile.name': 1}}),
+			Meteor.roles.find({})
+		];
+	}
+	return null;
 });
