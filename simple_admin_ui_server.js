35e5bfdd485d7fe91afef3fc17ea2414a937c49e
Cyrille Colin <cyrille.colin@univ-lorraine.fr>
2014-02-20 10:22:39 +0100

    patch the patch as i misunderstood brackets roles ...

--- simple_admin_ui_server.js
+++ simple_admin_ui_server.js
@@ -1,15 +1,22 @@
 Meteor.publish("allUsers", function() {
   if (Roles.userIsInRole(this.userId, ['admin'])) {
-    return 
-    [
-      Meteor.users.find({}, { 
+    return Meteor.users.find({}, {
+      fields : { 
         username : 1,
         profile : 1,
         roles : 1,
-        emails : 1,								  
-        sort: {'profile.name': 1}}),
-      Meteor.roles.find({})
-    ];
+        emails : 1
+      },
+      sort : {
+        'profile.name': 1
+      }
+    });
+  }
+  return null;
+});
+Meteor.publish(null, function (){
+  if (Roles.userIsInRole(this.userId, ['admin'])) {
+    return Meteor.roles.find({});
   }
   return null;
 });
